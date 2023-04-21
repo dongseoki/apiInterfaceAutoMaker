17e5b38bf3617e100c4462addf7a58f6a2358239
Dong Seok Lee <dongseok.lee.log@gmail.com>
2023-04-22 01:08:22 +0900

    더 진화중. 통계 자료 까지 프린트.

--- apiSpecGenerator.py
+++ apiSpecGenerator.py
@@ -1,5 +1,10 @@
 import json
 import os
+import sys
+import traceback
+
+global apNum
+apNum = 1
 
 
 def printByDepth(element, dataType, description, depth):
@@ -119,45 +124,160 @@ def getApiDesc(apiDatas, key, apiMN):
         return ''
 
 
+def replaceWithUnder(value):
+    return value.replace('/', '_')
+
+
+def makeAPSpecDirPath(uri, apiMN):
+    global apNum
+    path = './apiSpecGeneratorResult/' + 'AP_' + \
+        str(apNum).zfill(3) + '_' + replaceWithUnder(uri) + '_' + apiMN
+    return path
+
+
+def printAPIMeta(fileName, apiName, apiMN, apiDesc, key):
+    print('{}\n{}\n{}\n{}\n{}'.format(
+        fileName.split('.')[0], apiName, apiMN, apiDesc, key))
+
+
 def printApiSpec(relPath, fileName):
     f = open(relPath+'/' + fileName, encoding='utf8')
     data = json.load(f)
     apiDatas = data['paths']
     for key in sorted(list(data['paths'].keys())):
+        uri = key
+
         # print(key)
         apiMethodList = getApiMethods(apiDatas, key)
         for apiMN in apiMethodList:
+            fileToErrorDic[file_name]['apiSubCnt'] += 1
+            global apNum
+            createDirectory = makeAPSpecDirPath(uri, apiMN)
+            if not os.path.exists(createDirectory):
+                os.makedirs(createDirectory)
+            apNum += 1
+
             apiName = getApiName(apiDatas, key, apiMN)
             apiDesc = getApiDesc(apiDatas, key, apiMN)
-            print('{}\t{}\t{}\t{}\t{}'.format(
-                fileName.split('.')[0], apiName, apiMN, apiDesc, key))
-            print('api spec begin')
-            try:
-                printRequestParameter(key, apiMN, apiDatas)
-            except:
-                print('api spec request error occured', apiName, key)
+            original_stdout = sys.stdout
+            with open(createDirectory + '/api_meta_info.txt', 'w') as f:
+                # Change the standard output to the file we created.
+                sys.stdout = f
+                printAPIMeta(fileName, apiName, apiMN, apiDesc, key)
+                sys.stdout = original_stdout  # Reset the standard output to its original value
+            # print('api spec begin')
+            original_stdout = sys.stdout
+            with open(createDirectory + '/api_request_parameter.txt', 'w') as f:
+                # Change the standard output to the file we created.
+                sys.stdout = f
+                try:
+                    printRequestParameter(key, apiMN, apiDatas)
+                except:
+                    print('api spec request error occured', apiName, key)
+                    print(traceback.format_exc())
+                    fileToErrorDic[file_name]['errReqCnt'] += 1
+                    fileToErrorDic[file_name]['errReqList'].append(
+                        createDirectory)
+                sys.stdout = original_stdout  # Reset the standard output to its original value
+            with open(createDirectory + '/api_response_parameter.txt', 'w') as f:
+                # Change the standard output to the file we created.
+                sys.stdout = f
+                try:
+                    for statusCode in apiDatas[key][apiMN]['responses'].keys():
+                        if statusCode not in ['401', '404', '400', '500']:
+                            print('uri: {}, method: {}, stCode: {}, respone REsult begin'.format(
+                                key, apiMN, statusCode))
+                            printResponseByDepth(apiDatas[key][apiMN]['responses'][statusCode]['content']['application/json']['schema'],
+                                                 data['components']['schemas'])
+                            print('uri: {}, method: {}, stCode: {}, respone REsult END'.format(
+                                key, apiMN, statusCode))
 
-            try:
-                for statusCode in apiDatas[key][apiMN]['responses'].keys():
-                    if statusCode not in ['401', '404', '400', '500']:
-                        print('uri: {}, method: {}, stCode: {}, respone REsult begin'.format(
-                            key, apiMN, statusCode))
-                        printResponseByDepth(apiDatas[key][apiMN]['responses'][statusCode]['content']['application/json']['schema'],
-                                             data['components']['schemas'])
-                        print('uri: {}, method: {}, stCode: {}, respone REsult END'.format(
-                            key, apiMN, statusCode))
-
-                print('api spec end')
-            except:
-                print('api spec response result error occured', apiName, key)
-            print()
-            print('-' * 50)
-            print()
+                    print('api spec end')
+                except:
+                    print('api spec response result error occured', apiName, key)
+                    print(traceback.format_exc())
+                    fileToErrorDic[file_name]['errResCnt'] += 1
+                    fileToErrorDic[file_name]['errResList'].append(
+                        createDirectory)
+                sys.stdout = original_stdout  # Reset the standard output to its original value
+            # print()
+            # print('-' * 50)
+            # print()
 
 
 relPath = './jsonDatas'
-file_list = os.listdir(relPath)
+# file_list = os.listdir(relPath)
+file_list = ['common-api.json',
+             'banner-api.json',
+             'board-api.json',
+             'category-api.json',
+             'common-api2.json',
+             'consulting-api.json',
+             'contents-api.json',
+             'counseling-api.json',
+             'coupon-api.json',
+             'goods-api.json',
+             'inquiry-api.json',
+             'logging-api.json',
+             'login2-api.json',
+             'mailing-api.json',
+             'mavatar-api.json',
+             'mbody-api.json',
+             'mcloth-api.json',
+             'mface-api.json',
+             'mhair-api.json',
+             'micecontents-api.json',
+             'mregion-api.json',
+             'mshoes-api.json',
+             'mypage-api.json',
+             'order-api.json',
+             'popup-api.json',
+             'projectinfo-api.json',
+             'projectinfo2-api.json',
+             'quiz-api.json',
+             'session-api.json',
+             'stat2-api.json',
+             'statistics-api.json',
+             'subscribe-api.json',
+             'survey2-api.json',
+             'user-api.json']
+global fileToErrorDic
+fileToErrorDic = {}
+
 for file_name in file_list:
+    fileToErrorDic[file_name] = {
+        'apiSubCnt': 0,
+        'errReqCnt': 0,
+        'errResCnt': 0,
+        'errReqList': [],
+        'errResList': []
+    }
     printApiSpec(relPath, file_name)
 
 # printApiSpec('./jsonDatas', 'banner-api.json')
+
+totalErrReqCnt = 0
+totalErrResCnt = 0
+
+
+def printErrorInfo(file_name, errorInfo):
+    print('file_name: ', file_name)
+    print('apiSubCnt: ', errorInfo['apiSubCnt'])
+    print('errReqCnt: ', errorInfo['errReqCnt'])
+    print('errReqPercent: ',
+          errorInfo['errReqCnt'] / errorInfo['apiSubCnt'] * 100)
+    print('errResCnt: ', errorInfo['errResCnt'])
+    print('errResPercent: ',
+          errorInfo['errResCnt'] / errorInfo['apiSubCnt'] * 100)
+    print('errReqList: ', errorInfo['errReqList'])
+    print('errResList: ', errorInfo['errResList'])
+    print('-'*50)
+
+
+for file_name in file_list:
+    printErrorInfo(file_name, fileToErrorDic[file_name])
+    totalErrReqCnt += fileToErrorDic[file_name]['errReqCnt']
+    totalErrResCnt += fileToErrorDic[file_name]['errResCnt']
+
+print('totalErrReqCnt: ', totalErrReqCnt)
+print('totalErrResCnt: ', totalErrResCnt)
